acad6c1a34eafe22b23a6d188a8601bdab1ff70e
Tutorial Doctor <TutorialDoctor@users.noreply.github.com>
2015-09-14 18:03:33 -0400

    Create GameDesignDocument.gd

---
+++ More-Info/GameDesignDocument.gd
@@ -0,0 +1,86 @@
+**This is a summary of the game design document for the [Arcana](http://marceltau.com/files/Arcana_Project_GDD.pdf) project.**
+
+# Table of Contents: <a id="TOC"></a>
+
+[Game Description](#GD)
+
+[Functional Specifications](#FS)
+
+[User Interface](#UI)
+
+[Art & Video](#AV)
+
+[Sound & Music](#SM)
+
+[Story](#S)
+
+[Level Requirements](#LR)
+
+[Tehchnical Specifications](#TS)
+
+[Production Schedule](#PS)
+
+# Game Description <a id="GD"></a> ([Top](#TOC))
+* Design Goals
+* Influences & Sources
+* Target Market
+
+# Functional Specifications <a id="FS"></a>([Top](#TOC))
+* Game Mechanics
+* Core Game Play
+* Game Flow
+* Characters/Units
+* Game Play Elements
+* Game Physics and Statistics
+* Artificial Intelligence
+* Multiplayer
+
+#  User Interface <a id ="UI"></a>([Top](#TOC))
+* Flowchart
+* GUI Objects
+
+# Art & Video <a id="AV"></a>([Top](#TOC))
+* Overall Goals
+* 2D art & Animation
+* GUI
+* Marketing and Packaging Art
+* Terrain
+* Game Play Elements
+* Special Effects
+* 3D Art & Animation
+* Cinematic
+* Assets Pipeline
+
+# Sound & Music <a id="SM"></a>([Top](#TOC))
+* Overall Goals
+* Sound FX
+
+# Story <a id="S"></a>([Top](#TOC))
+* Player Characters
+* Secondary Characters
+* Enemy Characters
+* Story Theme
+* Visual Theme
+* Story Outline
+
+# Level Requirements <a id="LR"></a>([Top](#TOC))
+* Level Diagram
+* Asset Revelation Schedule
+* Level Design Seeds
+
+# Technical Specifications <a id="TS"></a>([Top](#TOC))
+* Game Mechanics
+* Game Engine
+* Platform & OS
+* External Code
+* Code Objects
+* Game Object Data
+* Data Flow
+* Artificial Intelligence
+
+# Production Schedule <a id="PS"></a>([Top](#TOC))
+* Scope
+* Scheduling
+* Dependencies
+* Cost Estimate
+
